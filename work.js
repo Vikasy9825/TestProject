d29f456039e60742fdf14c9337b51afbd23856c3
Vikas Yadav <vikasy.enjay@gmail.com>
2022-11-14 11:26:13 +0530

    js edited first

--- work.js
+++ work.js
@@ -1,1 +1,10 @@
-AbortController;
+const hour = new Date().getHours(); 
+let greeting;
+
+if (hour < 18) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+document.getElementById("demo").innerHTML = greeting;
